74b85cb0a36c3f2dc4f06784902940553d20f719
BijotVictor <victorbijot@gmail.com>
2020-11-24 19:41:39 +0100

    initial commit

---
+++ Speedometer.php
@@ -0,0 +1,19 @@
+<?php
+
+
+class Speedometer
+{
+
+    const milesToKm = 1.609;
+    const kmToMiles = 0.6215;
+
+
+    public static function kmToMiles($km)
+    {
+        return $km * self::kmToMiles;
+    }
+    public static function milesToKm($miles)
+    {
+        return $miles * self::milesToKm;
+    }
+}
